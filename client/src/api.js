c7258ff0b3862a874b2fbe6a92174da4afff47b1
stylesdurgesh123 <156895389+stylesdurgesh123@users.noreply.github.com>
2025-11-03 21:37:22 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -2,8 +2,7 @@ import axios from 'axios'
 
 const BASE_URL = "https://crypto-tracker-app-3.onrender.com";
 
-
-const client = axios.create({ baseURL })
+const client = axios.create({ baseURL: BASE_URL });
 
 export async function getCoins() {
   const { data } = await client.get('/api/coins')
